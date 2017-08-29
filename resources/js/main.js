8f82e16b3653e2aefc5cce6f0914b66aa0244f60
Shovan Chatterjee <shovanc.web@gmail.com>
2017-08-30 03:16:50 +0530

    add clearCompleted, count functionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,7 +3,10 @@ const localDay = document.getElementById('day');
 const localDate = document.getElementById('date');
 const todoInput = document.querySelector('input');
 const ul = document.getElementById('todoList');
-const li = document.querySelector('ul');
+const deleteAll = document.createElement('button');
+deleteAll.className = 'deleteAllBtn';
+const counter = document.createElement('span');
+let count = 0; // Global counter Variable for todo count tracking
 
 // Function to display date and time using Date() objecy
 function showDate () {
@@ -19,7 +22,10 @@ function showDay () {
 localDay.innerHTML = showDay();
 localDate.innerHTML = showDate();
 
-// Gets input from user and output the list element
+/*
+  EventListener on input to get user input
+  Gets input from user and output the list element
+*/
 todoInput.addEventListener('keyup', function (e) {
   if (e.keyCode === 13) {
     const li = document.createElement('li');
@@ -29,30 +35,66 @@ todoInput.addEventListener('keyup', function (e) {
     span.textContent = text;
     li.appendChild(span);
     const button = document.createElement('button');
+    button.className = 'deleteButton';
     button.textContent = 'Done?';
     li.appendChild(button);
     ul.appendChild(li);
+
+    // increment count if new todo is added
+    count++;
+    counter.innerHTML = 'You have ' + count + ' items to do';
+    ul.appendChild(counter);
+
+    // append the clear Completed button
+    deleteAll.textContent = 'Clear Completed';
+    ul.appendChild(deleteAll);
   }
 });
 
-// toggle done and delete todo
-li.addEventListener('click', function (e) {
+/*
+  Event listeners on the todo items
+*/
+ul.addEventListener('click', function (e) {
+  // on the todo text, to toggle done
   if (e.target.tagName === 'SPAN') {
-    e.target.classList.toggle('done');
-  }
-  if (e.target.tagName === 'BUTTON') {
-    const list = e.target.parentNode;
-    const ul = list.parentNode;
-    ul.removeChild(list);
-  }
-  // prints out number of done todos
-  const lis = li.children.children;
-  let count = 0;
-  for (let i = 0; i < lis.length; i++) {
-    let lists = lis[i];
-    if (lists.className === 'done') {
+    const span = e.target;
+    e.target.parentNode.classList.toggle('hey');
+    span.classList.toggle('done');
+
+    // decrement or increment, depending on toggle class
+    if (e.target.parentNode.className === 'hey') {
+      count--;
+    } else {
       count++;
     }
+
+    counter.innerHTML = 'You have ' + count + ' items to do';
+    ul.appendChild(counter);
   }
-  console.log(count);
-}, false);
+
+  // on the delete button, to delete individual todo
+  if (e.target.className === 'deleteButton') {
+    const button = e.target;
+    const list = button.parentNode;
+    const mainLi = list.parentNode;
+
+    // if done, then decrement
+    if (list.className !== 'hey') {
+      count--;
+    }
+
+    mainLi.removeChild(list);
+    counter.innerHTML = 'You have ' + count + ' items to do';
+    ul.appendChild(counter);
+  }
+});
+
+// on the clear Completed button, to clear all Completed todos
+deleteAll.addEventListener('click', function () {
+  for (let i = 0; i < ul.children.length; i++) {
+    let listElem = ul.children[i];
+    if (listElem.className === 'hey') {
+      ul.removeChild(listElem);
+    }
+  }
+});
